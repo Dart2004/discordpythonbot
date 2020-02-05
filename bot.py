32714d0e9aa2f7905c3f61993c59e10493582e00
Dart2004 <59048496+Dart2004@users.noreply.github.com>
2020-02-05 12:42:38 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,26 +1,35 @@
-  
-{
-
-  "name": "try",
-
-  "description": "try",
-
-  "version": "0.0.0",
-
-  "main": "bot.py",
-
-  "scripts": {
-
-    "start": "python bot.py"
-
-  },
-
-  "dependencies": {
-
-    "discord.py": "11.1.0",
-
-    "request": "2.81.0"
-
-  }
-
-}
+import discord
+client=discord.Client()
+import dic
+import time
+client.prefix=">"
+know=dict(dic.dicti)
+g=False
+@client.event
+async def on_message(message):
+	global g,know,dic,question,answer
+	if message.author==client.user:
+		return
+	message.content=message.content.lower()
+	if client.prefix in message.content:
+		try:
+			if message.content=="runtime":
+				await message.channel.send(time.process_time())
+				return
+			await message.channel.send(know[message.content.replace(client.prefix,"")])
+		except:
+			pass
+	elif g==False:
+		print(message.content)
+		g=True
+		question=message.content
+	elif g==True:
+		print(message.content)
+		g=False
+		answer=message.content
+		if question not in know.keys():
+			print(str(know))
+			know[question]=answer
+			d=open("dic.py","w")
+			d.write("dicti="+str(know))
+client.run("NjY0NzM4MDU5ODExNTUzMjgy.XjW9bg.iNeqiw2WRgYZncmhdbAf_7A0PcM")
